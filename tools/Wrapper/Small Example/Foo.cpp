de7127c7a7d56aa0d20f68762f42f8dbc635aea4
Matthias Walter <matthias@matthiaswalter.org>
2017-01-29 21:48:43 +0100

    Removed old cython code.

--- tools/Wrapper/Small Example/Foo.cpp
+++
@@ -1,9 +0,0 @@
-#include "Foo.h"
-
-Foo::Foo(){
-    printf("Create Foo\n");
-}
-
-std::string Foo::printFoo(){
-	return "I am a Test!";
-}
--- tools/Wrapper/Small Example/Foo2.cpp
+++
@@ -1,9 +0,0 @@
-#include "Foo2.h"
-
-Foo2::Foo2(){
-    printf("Create Foo\n");
-}
-
-std::string Foo2::printFoo2(){
-	return "I am a Test!";
-}
