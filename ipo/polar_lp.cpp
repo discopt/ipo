500b0bc48a8652d42cfe439c23ec1232b7d2cf49
Matthias Walter <matthias@matthiaswalter.org>
2017-10-06 10:32:24 +0200

    Removed debug output

--- ipo/polar_lp.cpp
+++ ipo/polar_lp.cpp
@@ -166,9 +166,7 @@ namespace ipo {
     {
       notify(PolarLPHandler::LP_BEGIN);
 
-      std::cerr << "<solve>" << std::endl;
       SPxSolver::Status status = _spx->solve();
-      std::cerr << "</solve>" << std::endl;
 
       if (status == SPxSolver::OPTIMAL)
       {
